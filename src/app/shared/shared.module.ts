f4a4969cfbcea4da87ae51a1d10ac1308fed486e
Samira Bittar <olhosespanhois@hotmail.com>
2023-07-21 18:01:29 -0300

    Shared module

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+
+@NgModule({
+    declarations: [],
+    providers: [],
+    bootstrap: [],
+    imports: [],
+    exports: [],
+})
+export class SharedModule { }
