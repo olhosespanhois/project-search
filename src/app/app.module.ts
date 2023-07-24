e86e1cf831e9065659309d86845be0c565a70a0d
Samira Bittar <olhosespanhois@hotmail.com>
2023-07-23 22:25:51 -0300

    modal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,25 +3,26 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
-
 @NgModule({
-    declarations: [
-        AppComponent
-    ],
-    providers: [],
-    bootstrap: [AppComponent],
-    imports: [
-        BrowserModule,
-        FormsModule,
-        ReactiveFormsModule,
-        HttpClientModule,
-        AppRoutingModule,
-        CoreModule,
-        SharedModule,
-    ]
+	declarations: [
+		AppComponent
+	],
+	providers: [],
+	bootstrap: [AppComponent],
+	imports: [
+		BrowserModule,
+		FormsModule,
+		ReactiveFormsModule,
+		HttpClientModule,
+		NgbModule,
+		AppRoutingModule,
+		CoreModule,
+		SharedModule
+	]
 })
 export class AppModule { }
